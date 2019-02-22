67fe41b022c29a2aa318b082b8b62d4e328c9624
Selçuk SARIKÖZ <selcuk@pankod.com>
2019-02-22 15:07:54 +0300

    react native i18n integration

---
+++ src/App/I18n/Languages.ts
@@ -0,0 +1,2 @@
+export * from '@App/I18n/TR-TR';
+export * from '@App/I18n/EN-US';
---
+++ src/App/I18n/index.ts
@@ -0,0 +1,13 @@
+import I18nClient from 'react-native-i18n';
+
+import { EN, TR } from '@App/I18n/Languages';
+
+I18nClient.fallbacks = true;
+I18nClient.locale = 'tr';
+
+I18nClient.translations = {
+	en: EN,
+	tr: TR
+};
+
+export let I18n = I18nClient;
