1771317aae366f11c6d9c3bcbb789637d16b0aa9
aydin <aydinakyol11@gmail.com>
2019-09-12 10:58:52 +0300

    index js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import { AppRegistry } from 'react-native';
-import { App } from './src/App/App';
+import App from './src/App';
 import { name as appName } from './app.json';
 
 // To see all the requests in the chrome Dev tools in the network tab.
 XMLHttpRequest = GLOBAL.originalXMLHttpRequest ?
-	GLOBAL.originalXMLHttpRequest :
-	GLOBAL.XMLHttpRequest;
+    GLOBAL.originalXMLHttpRequest :
+    GLOBAL.XMLHttpRequest;
 
 global._fetch = fetch;
 global.fetch = function (uri, options, ...args) {
-	return global._fetch(uri, options, ...args).then((response) => {
-		console.log('Fetch', { request: { uri, options, ...args }, response });
-		return response;
-	});
+    return global._fetch(uri, options, ...args).then((response) => {
+        console.log('Fetch', { request: { uri, options, ...args }, response });
+        return response;
+    });
 };
-
 AppRegistry.registerComponent(appName, () => App);
