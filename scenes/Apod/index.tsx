4c9dfb9fe21947a40ebc7ba46bd36c50af9a3e3b
aydin <aydinakyol11@gmail.com>
2019-09-12 17:52:19 +0300

    Apod scene

---
+++ scenes/Apod/index.tsx
@@ -0,0 +1,37 @@
+// Global Imports
+import React, { Component } from 'react';
+import { Text, Svg } from 'react-native-svg';
+import { connect } from 'react-redux';
+
+// Local Styles
+import { Container } from '@Styled/Home';
+import { ContainerCenter } from '@Styled';
+
+
+class ApodComponent extends Component {
+    public render(): JSX.Element {
+        return (
+            <Container>
+                <ContainerCenter>
+                    <Svg height="100" width="200">
+                        <Text
+                            fill="white"
+                            stroke="white"
+                            fontSize="30"
+                            fontWeight="bold"
+                            x="100"
+                            y="30"
+                            textAnchor="middle"
+                        >   Apod Available!
+                    </Text>
+                    </Svg>
+                </ContainerCenter>
+
+            </Container>
+        );
+    }
+}
+
+const Apod = connect()(ApodComponent);
+
+export default Apod;
