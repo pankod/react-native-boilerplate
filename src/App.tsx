1eb87e0adbfbe2118929fe32e30c3b4961600003
aydin <aydinakyol11@gmail.com>
2019-09-12 14:57:09 +0300

    Store and Assets path refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 // Global Imports
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { View, Text } from 'react-native';
 // import SplashScreen from 'react-native-splash-screen';
 // import codePush from "react-native-code-push";
 
 // Local Imports
 import AppContainer from '@Router';
-import configureStore from '@Store';
+import configureStore from '@Redux/store';
 import RouterActions from '@Services/RouterActions';
 
 // Local Styles
-import { SafeArea } from '@Assets/Styles';
+import { SafeArea } from '@Styled';
 
 // Configure Store
 const store = configureStore({});
