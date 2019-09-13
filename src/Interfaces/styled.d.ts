acc13c0c392bfc0f591a7754c3c9e67b9fc5e56d
aydin <aydinakyol11@gmail.com>
2019-09-13 11:24:26 +0300

    remove export

--- src/Interfaces/styled.d.ts
+++ src/Interfaces/styled.d.ts
@@ -1,7 +1,7 @@
 import "styled-components";
 
 declare module "styled-components" {
-    export interface DefaultTheme {
+    interface DefaultTheme {
         colors: {
             primary: string;
         };
