9156db3a687eb52778fed94be37296191a6790ce
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2019-09-13 12:26:45 +0300

    Update eslint config, add missing libs, types

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    root: true,
+    extends: [
+        "@react-native-community",
+        "airbnb-typescript",
+        "prettier",
+        "prettier/@typescript-eslint",
+        "prettier/react",
+    ],
+    rules: {
+        "import/no-unresolved": "off"
+    }
 };
