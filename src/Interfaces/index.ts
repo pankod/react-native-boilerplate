6f683d59f635fd02293a9c1b7a5439c168bcb1a2
aydin <aydinakyol11@gmail.com>
2019-09-13 11:24:17 +0300

    interfaces updated

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -4,3 +4,10 @@ export * from "@Interfaces/Scenes/Home";
 // Redux Interfaces
 export * from "@Redux/IAction";
 export * from "@Redux/IStore";
+
+// #region Service Interfaces
+export * from "@Services/API/Http/Http";
+export * from "@Services/API/Planetary/ApodPayload";
+export * from "@Services/API/Planetary/ApodResponse";
+export * from "@Services/API/Planetary/Planetary";
+// #endregion Service Interfaces
