169aa2991f06fae28c3da5709ee024f285b78111
aydin <aydinakyol11@gmail.com>
2019-09-16 15:33:29 +0300

    dimensions added to DefaultTheme

--- src/Definitions/Styled/theme.ts
+++ src/Definitions/Styled/theme.ts
@@ -1,8 +1,16 @@
 import { DefaultTheme } from "styled-components/native";
+import { Dimensions } from "react-native";
+
+const screenWidth = Math.round(Dimensions.get("window").width);
+const screenHeight = Math.round(Dimensions.get("window").height);
 
 const theme: DefaultTheme = {
     colors: {
         primary: "#2c3e50",
+    },
+    dimensions: {
+        screenWidth,
+        screenHeight,
     },
 };
 
