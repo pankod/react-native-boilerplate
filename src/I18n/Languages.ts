7b211606d6634752c6dc1a71b094e417337598f7
aydin <aydinakyol11@gmail.com>
2019-09-16 10:54:23 +0300

    read lang from json

--- src/I18n/Languages.ts
+++ src/I18n/Languages.ts
@@ -1,2 +1,3 @@
-export * from '@App/I18n/TR-TR';
-export * from '@App/I18n/EN-US';
+export { default as en } from "@I18n/locales/en/common.json";
+export { default as tr } from "@I18n/locales/tr/common.json";
+export { default as es } from "@I18n/locales/es/common.json";
