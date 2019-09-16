196f452eb27a614b28a2001de9e9b95f3f6593cc
aydin <aydinakyol11@gmail.com>
2019-09-16 10:54:11 +0300

    refactor I18n

--- src/I18n/index.ts
+++ src/I18n/index.ts
@@ -1,14 +1,37 @@
 // Global imports
-import I18nClient from 'react-native-i18n';
+import { initReactI18next } from "react-i18next";
 // Local imports
-import { EN, TR } from '@App/I18n/Languages';
+import i18next from "i18next";
 
-I18nClient.fallbacks = true;
-I18nClient.locale = 'tr';
+import { en, tr, es } from "@I18n/Languages";
 
-I18nClient.translations = {
-	en: EN,
-	tr: TR,
+console.log("re", tr);
+
+const languageDetector = {
+    type: "languageDetector",
+    async: true,
+    detect: cb => cb("en"),
+    init: () => {},
+    cacheUserLanguage: () => {},
 };
 
-export const I18n = I18nClient;
+i18next
+    .use(languageDetector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: "en",
+        debug: true,
+        resources: {
+            en: {
+                translation: en,
+            },
+            tr: {
+                translation: tr,
+            },
+            es: {
+                translation: es,
+            },
+        },
+    });
+
+export const I18n = i18next;
