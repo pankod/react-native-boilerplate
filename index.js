9c267cd397e26a5689fc308a5a806e5ad0f61fd7
aydin <aydinakyol11@gmail.com>
2019-09-16 15:15:30 +0300

    remove global fetch track

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-import { AppRegistry } from 'react-native';
-import App from './src/App';
-import { name as appName } from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./src/App";
+import { name as appName } from "./app.json";
 
-// To see all the requests in the chrome Dev tools in the network tab.
-XMLHttpRequest = GLOBAL.originalXMLHttpRequest ?
-    GLOBAL.originalXMLHttpRequest :
-    GLOBAL.XMLHttpRequest;
-
-global._fetch = fetch;
-global.fetch = function (uri, options, ...args) {
-    return global._fetch(uri, options, ...args).then((response) => {
-        console.log('Fetch', { request: { uri, options, ...args }, response });
-        return response;
-    });
-};
 AppRegistry.registerComponent(appName, () => App);
