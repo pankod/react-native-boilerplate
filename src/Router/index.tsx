630cc6afd95eec686762702614596957e34866f7
aydin <aydinakyol11@gmail.com>
2019-09-17 13:56:50 +0300

    headerStyle added to react navigation

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,28 +1,35 @@
 // Global Imports
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
 // Local Imports
-import Home from '@Scenes/Home';
-import Apod from '@Scenes/Apod';
+import { theme } from "@Definitions/Styled";
 
-const AppStackNavigator = createStackNavigator(
-    {
+import Home from "@Scenes/Home";
+import Apod from "@Scenes/Apod";
 
-        Home: {
-            navigationOptions: ({ navigation }) => ({
-                title: 'Home',
-            }),
-            screen: Home,
-        },
-        Apod: {
-            navigationOptions: ({ navigation }) => ({
-                title: 'Apod',
-            }),
-            screen: Apod,
-        },
-    }
-);
+const AppStackNavigator = createStackNavigator({
+    Home: {
+        navigationOptions: ({ navigation }) => ({
+            title: "Home",
+            headerStyle: {
+                backgroundColor: theme.colors.primary,
+            },
+            headerTintColor: "#fff",
+        }),
+        screen: Home,
+    },
+    Apod: {
+        navigationOptions: ({ navigation }) => ({
+            title: "Apod",
+            headerStyle: {
+                backgroundColor: theme.colors.primary,
+            },
+            headerTintColor: "#fff",
+        }),
+        screen: Apod,
+    },
+});
 
 const AppContainer = createAppContainer(AppStackNavigator);
 
