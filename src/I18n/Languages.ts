f098d3328296ab1e1707661879ba8f86fd6d71e1
aydin <aydinakyol11@gmail.com>
2019-09-17 13:30:50 +0300

    remove deleted files

--- src/I18n/Languages.ts
+++
@@ -1,3 +0,0 @@
-export { default as en } from "@I18n/locales/en/common.json";
-export { default as tr } from "@I18n/locales/tr/common.json";
-export { default as es } from "@I18n/locales/es/common.json";
