9401a3b6788b360dd8f2c0a5103d622acf63b19f
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2019-09-19 13:16:18 +0300

    renderWithTheme wraps for I18n

--- test/Helpers/styled.tsx
+++ test/Helpers/styled.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
 import { theme } from "@Definitions/Styled";
 import { ThemeProvider } from "styled-components/native";
+import { I18nextProvider } from "react-i18next";
 import renderer from "react-test-renderer";
+import { I18n } from "../../src/I18n/index";
 
 export function renderWithTheme(child: React.ReactElement) {
-    return renderer.create(
-        <ThemeProvider theme={theme}>{child}</ThemeProvider>
-    ).toJSON();
+    return renderer
+        .create(
+            <I18nextProvider i18n={I18n}>
+                <ThemeProvider theme={theme}>{child}</ThemeProvider>
+            </I18nextProvider>
+        )
+        .toJSON();
 }
