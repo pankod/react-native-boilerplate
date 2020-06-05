b1e9783d2d232941e418f1764a8f147338db9c32
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2020-06-05 15:05:48 +0300

    add post service mock

--- src/Services/Post/index.spec.ts
+++ src/Services/Post/index.spec.ts
@@ -1,12 +1,8 @@
-import nock from "nock";
-
 import { PostService } from "@Services";
 
 describe("Services", () => {
     describe("PostService", () => {
         it("should get posts", async () => {
-            nock.recorder.rec();
-
             const response = await PostService.List();
 
             expect(response).toBeDefined();
