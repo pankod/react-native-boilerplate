395deaec0ce946bc548b9ef1ec0fcee4e79c7461
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2020-06-08 10:55:39 +0300

    add svgr

--- src/Screens/Settings/index.tsx
+++ src/Screens/Settings/index.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { View, Text, ScrollView, SafeAreaView } from "react-native";
 import { useTranslation } from "react-i18next";
 
+import { Settings as SettingsIcon } from "@Components/Icons";
+
 export const Settings: React.FC = () => {
     const { t } = useTranslation();
 
@@ -9,6 +11,7 @@ export const Settings: React.FC = () => {
         <SafeAreaView>
             <ScrollView>
                 <View>
+                    <SettingsIcon width="50" height="50" fill="red" />
                     <Text>{t("common:Hello")}</Text>
                 </View>
             </ScrollView>
